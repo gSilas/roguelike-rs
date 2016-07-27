f57c7385e39805cec2900ea231901c425ed1a25c
gSilas <d.micheel@outlook.com>
2016-07-27 14:43:36 +0200

    part 2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,50 @@
 extern crate tcod;
 
 use tcod::console::*;
-use tcod::colors;
-use tcod::input::Key;
-use tcod::input::KeyCode::*;
+use tcod::colors::{self, Color};
 
 // const window modifiers
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
-const LIMIT_FPS: i32 = 20;
+const LIMIT_FPS: i32 = 60;
+
+// basic game Object
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+// implementaion of game Object
+impl Object {
+    // new creates gaem Object
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object {
+            x: x,
+            y: y,
+            char: char,
+            color: color,
+        }
+    }
+
+    // move by dx and dy
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    // set color and draw character
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+
+    // erase character
+    pub fn clear(&self, con: &mut Console) {
+        con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+}
 
 // main loop
 fn main() {
@@ -21,23 +57,47 @@ fn main() {
         .title("libtcod-rs Roguelike")
         .init();
 
+    // offscreen console
+    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+
     // limits loop times
     tcod::system::set_fps(LIMIT_FPS);
 
-    // player location
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+    // player
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
 
-    // draw loop
+    // NPC
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
+
+    // list of those two
+    let mut objects = [player, npc];
+
+
+    // main loop
     while !root.window_closed() {
-        root.set_default_foreground(colors::WHITE);
-        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
-        root.flush();
-        root.wait_for_keypress(true);
+        // draw all objects in the list
+        for object in &objects {
+            object.draw(&mut con);
+        }
 
-        // using input handling
-        root.put_char(player_x, player_y, ' ', BackgroundFlag::None);
-        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        // blit the contents of "con" to the root console and present it
+        blit(&mut con,
+             (0, 0),
+             (SCREEN_WIDTH, SCREEN_HEIGHT),
+             &mut root,
+             (0, 0),
+             1.0,
+             1.0);
+        root.flush();
+
+        // erase all objects at their old locations, before they move
+        for object in &objects {
+            object.clear(&mut con)
+        }
+
+        // handle keys and exit game if needed
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut root, player);
         if exit {
             break;
         }
@@ -45,21 +105,27 @@ fn main() {
 }
 
 // Input handling
-fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
     let key = root.wait_for_keypress(true);
     match key {
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
-        // alt+enter = fullscreen
         Key { code: Enter, alt: true, .. } => {
+            // Alt+Enter: toggle fullscreen
             let fullscreen = root.is_fullscreen();
             root.set_fullscreen(!fullscreen);
         }
-        // esc = exit
-        Key { code: Escape, .. } => return true,
+        Key { code: Escape, .. } => return true,  // exit game
+
+        // movement keys
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
+
         _ => {}
     }
+
     false
 }
