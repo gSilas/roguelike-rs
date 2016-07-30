cd5dbc38617eafbcc18278535c814fa8828256c0
dan <dan@thinkpad.fritz.box>
2016-07-30 10:24:59 +0200

    map part

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate tcod;
 
+use std::cmp;
+
 use tcod::console::*;
 use tcod::colors::{self, Color};
 
@@ -8,7 +10,100 @@ const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 60;
 
+// map modifiers
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+const COLOR_DARK_WALL: Color = Color {
+    r: 0,
+    g: 0,
+    b: 100,
+};
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150,
+};
+
+#[derive(Clone,Copy,Debug)]
+struct Rect {
+    x1: i32,
+    x2: i32,
+    y1: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    // horizontal tunnel
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    // vertical tunnel
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+// tile
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+type Map = Vec<Vec<Tile>>;
+
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    // 2 rooms
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
+
+    map
+}
+
 // basic game Object
+#[derive(Debug)]
 struct Object {
     x: i32,
     y: i32,
@@ -16,7 +111,6 @@ struct Object {
     color: Color,
 }
 
-// implementaion of game Object
 impl Object {
     // new creates gaem Object
     pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
@@ -29,9 +123,11 @@ impl Object {
     }
 
     // move by dx and dy
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    pub fn move_by(&mut self, dx: i32, dy: i32, map: &Map) {
+        if !map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
     }
 
     // set color and draw character
@@ -58,13 +154,13 @@ fn main() {
         .init();
 
     // offscreen console
-    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
     // limits loop times
     tcod::system::set_fps(LIMIT_FPS);
 
     // player
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let player = Object::new(25, 23, '@', colors::WHITE);
 
     // NPC
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
@@ -72,22 +168,14 @@ fn main() {
     // list of those two
     let mut objects = [player, npc];
 
+    // map
+    let map = make_map();
 
     // main loop
     while !root.window_closed() {
-        // draw all objects in the list
-        for object in &objects {
-            object.draw(&mut con);
-        }
 
-        // blit the contents of "con" to the root console and present it
-        blit(&mut con,
-             (0, 0),
-             (SCREEN_WIDTH, SCREEN_HEIGHT),
-             &mut root,
-             (0, 0),
-             1.0,
-             1.0);
+        render_all(&mut root, &mut con, &objects, &map);
+
         root.flush();
 
         // erase all objects at their old locations, before they move
@@ -97,15 +185,33 @@ fn main() {
 
         // handle keys and exit game if needed
         let player = &mut objects[0];
-        let exit = handle_keys(&mut root, player);
+        let exit = handle_keys(&mut root, player, &map);
         if exit {
             break;
         }
     }
 }
 
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    // draw all objects in the list
+    for object in objects {
+        object.draw(con);
+    }
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = map[x as usize][y as usize].block_sight;
+            if wall {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set)
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set)
+            }
+        }
+    }
+    // blit the contents of "con" to the root console and present it
+    blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
+}
 // Input handling
-fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
+fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -119,10 +225,10 @@ fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
         Key { code: Escape, .. } => return true,  // exit game
 
         // movement keys
-        Key { code: Up, .. } => player.move_by(0, -1),
-        Key { code: Down, .. } => player.move_by(0, 1),
-        Key { code: Left, .. } => player.move_by(-1, 0),
-        Key { code: Right, .. } => player.move_by(1, 0),
+        Key { code: Up, .. } => player.move_by(0, -1, map),
+        Key { code: Down, .. } => player.move_by(0, 1, map),
+        Key { code: Left, .. } => player.move_by(-1, 0, map),
+        Key { code: Right, .. } => player.move_by(1, 0, map),
 
         _ => {}
     }
